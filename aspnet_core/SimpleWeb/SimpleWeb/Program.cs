060b7caf142b49f54ecaae619f6901075f789a52
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2026-03-30 11:46:12 +0530

    day#10 update#1

--- aspnet_core/SimpleWeb/SimpleWeb/Program.cs
+++ aspnet_core/SimpleWeb/SimpleWeb/Program.cs
@@ -1,3 +1,5 @@
+using SimpleWeb.Utils;
+
 namespace SimpleWeb
 {
     public class Program
@@ -9,6 +11,38 @@ namespace SimpleWeb
 
 
 
+            app
+                .UseOnUrl("/", async context => "<h1>Welcome to The Bank</h1>" )
+                .UseOnUrl("/date",
+                          async context => $"<h1>Today is {DateTime.Now.ToLongDateString()}</h1>" , 
+                          PathMatcher.StartsWith)
+                .UseOnUrl("/time", async context =>
+                {
+                    await context.Response.WriteAsync($"<h1>Time Now is {DateTime.Now.ToLongTimeString()}</h1>");
+                }, PathMatcher.Contains);
+
+            
+            
+            //RouteSet03(app);
+            //RouteSet01(app);
+            //HandleMultipleAcctions(app);
+
+            app.Run();
+        }
+
+        private static void RouteSet03(WebApplication app)
+        {
+            app.UseOnUrl("/", async context =>
+            {
+                await context.Response.WriteAsync("<h1>Welcome to The Bank</h1>");
+            });
+
+            Middlewares.UseOnUrl(app, "/time", async context =>
+            {
+                await context.Response.WriteAsync(DateTime.Now.ToLongTimeString());
+            });
+
+
             app.Use(async (context, next) =>
             {
                 if (context.Request.Path == "/date")
@@ -16,35 +50,32 @@ namespace SimpleWeb
                 else
                     await next(context); //call the next item
             });
+        }
 
+        private static void HandleMultipleAcctions(WebApplication app)
+        {
+            app.Run(async context =>
+            {
+                var path = context.Request.Path.ToString();
 
+                switch (path)
+                {
+                    case "/":
+                        await context.Response.WriteAsync("Welcome to 'The Bank' ");
+                        break;
+                    case "/date":
+                        await context.Response.WriteAsync($"Today is {DateTime.Now.ToLongDateString()}");
+                        break;
+                    case "/time":
+                        await context.Response.WriteAsync($"Time now is {DateTime.Now.ToLongTimeString()}");
+                        break;
+                    default:
+                        context.Response.StatusCode = 404;
+                        await context.Response.WriteAsync($"Not Found:  {context.Request.Method} {path}");
+                        break;
+                }
 
-            //RouteSet01(app);
-
-            //app.Run(async context =>
-            //{
-            //    var path = context.Request.Path.ToString();
-
-            //    switch(path)
-            //    {
-            //        case "/":
-            //            await context.Response.WriteAsync("Welcome to 'The Bank' ");
-            //            break;
-            //        case "/date":
-            //            await context.Response.WriteAsync($"Today is {DateTime.Now.ToLongDateString()}");
-            //            break;
-            //        case "/time":
-            //            await context.Response.WriteAsync($"Time now is {DateTime.Now.ToLongTimeString()}");
-            //            break;
-            //        default:
-            //            context.Response.StatusCode = 404;
-            //            await context.Response.WriteAsync($"Not Found:  {context.Request.Method} {path}");
-            //            break;
-            //    }
-
-            //});
-
-            app.Run();
+            });
         }
 
         private static void RouteSet01(WebApplication app)
---
+++ aspnet_core/SimpleWeb/SimpleWeb/Utils/Middlewares.cs
@@ -0,0 +1,93 @@
+﻿namespace SimpleWeb.Utils
+{
+
+    public class PathMatcher
+    {
+        public static bool  Contains(HttpRequest request, string url)
+        {
+            return request.Path.ToString().ToLower().Contains(url.ToLower());
+        }
+
+        public static  bool StartsWith(HttpRequest request, string url)
+        {
+            return request.Path.ToString().ToLower().StartsWith(url.ToLower());
+        }
+
+        public static bool EndsWith(HttpRequest request, string url)
+        {
+            return request.Path.ToString().ToLower().EndsWith(url.ToLower());
+        }
+
+        public static bool Matches(HttpRequest request, string url)
+        {
+            return request.Path.ToString().ToLower() == url.ToLower();
+        }
+
+    }
+
+
+    public static class Middlewares
+    {
+        public static WebApplication UseOnUrl(this WebApplication app, string url, RequestDelegate action, Func<HttpRequest,string, bool> matcher=null)
+        {
+            app.Use(async (context, next) =>
+            {
+                if (matcher == null)
+                    matcher = PathMatcher.Matches;
+
+                if (matcher(context.Request,url))
+                    await action(context);
+                else
+                    await next(context);
+
+            });
+
+            return app;
+        }
+
+        public static WebApplication UseOnUrl(this WebApplication app, string url, Func<HttpContext,Task<string>> action, Func<HttpRequest, string, bool> matcher = null)
+        {
+            app.Use(async (context, next) =>
+            {
+                if (matcher == null)
+                    matcher = PathMatcher.Matches;
+
+                if (matcher(context.Request, url))
+                {
+                    var result = await action(context);
+                    await context.Response.WriteAsync(result);
+                }
+                else
+                    await next(context);
+
+            });
+
+            return app;
+        }
+
+
+        public static WebApplication  UseBefore(this WebApplication app, RequestDelegate action)
+        {
+            app.Use(async (context, next) =>
+            {
+                await action(context);
+                await next(context);
+            });
+
+            return app;
+        }
+
+        public static WebApplication UseAfter(this WebApplication app, RequestDelegate action)
+        {
+            app.Use(async (context, next) =>
+            {
+                await next(context);
+                await action(context);
+            });
+
+            return app;
+        }
+
+
+    }
+}
