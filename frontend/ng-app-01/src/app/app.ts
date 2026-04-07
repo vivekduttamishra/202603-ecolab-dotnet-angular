d2df475e4c7a837fdae45f7a4e13c94bfc8ae117
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2026-04-07 15:52:14 +0530

    day#16 update#2

---
+++ frontend/ng-app-01/src/app/app.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "the-bank",
+
+    template: `
+        <h1>The Bank</h1>
+        <p>Welcome to The Bank—the only bank you need to bank with.</p>
+        `,
+
+   styles: `
+    h1{
+        color: cadetblue;
+    }
+
+    p{
+        color:cadetblue;
+    }
+
+   `
+
+})
+export class App {
+
+}
